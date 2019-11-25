d6fe415a5b57b8d57c41c46ea2c775a9aecbc5af
David J. M. Karlsen <david@davidkarlsen.com>
2019-11-26 00:26:06 +0100

    fix version

--- test/e2e-kind.sh
+++ test/e2e-kind.sh
@@ -5,7 +5,7 @@ set -o nounset
 set -o pipefail
 
 readonly CT_VERSION=v2.4.0
-readonly KIND_VERSION=0.6.0
+readonly KIND_VERSION=v0.6.0
 readonly CLUSTER_NAME=chart-testing
 readonly K8S_VERSION=v1.16.3
 
